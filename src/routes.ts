0fb23fc6b0d0f8b5acf5fed1af7a22de6b7665d2
pedroblome <joaopedroblome78@gmail.com>
2022-09-07 00:22:47 -0300

    #139 refactor: Adcionado rota para criar equipamento

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import { adaptExpressRoute as adapt } from './adapters/express-router'
 import { makeCreateOrderController } from './factories/controllers/create-order-service'
+import { makeCreateEquipmentController } from './factories/controllers/createEquipment'
 
 const routes = Router()
 
 routes.post(
   '/create-order-service/:equipmentId',
   adapt(makeCreateOrderController())
+)
+routes.post(
+  '/createEquipment',
+  adapt(makeCreateEquipmentController())
 )
 
 export default routes
