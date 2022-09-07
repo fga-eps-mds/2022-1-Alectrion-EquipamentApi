76e8253b45755778eaf1a11350c792a1d92d30e8
pedroblome <joaopedroblome78@gmail.com>
2022-09-07 00:24:34 -0300

    #139 shore: Criado UseCase e Controller Factories

---
+++ src/factories/controllers/createEquipment.ts
@@ -0,0 +1,6 @@
+import { CreateEquipmentController } from "../../presentation/controller/createEquipmentController"
+import { makeCreateEquipment } from "../useCases/createEquipment"
+
+export const makeCreateEquipmentController = () =>{
+  return new CreateEquipmentController(makeCreateEquipment())
+}
