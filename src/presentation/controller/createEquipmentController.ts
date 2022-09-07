ed5cae03575e1df895b434130c12b0a15564006a
pedroblome <joaopedroblome78@gmail.com>
2022-09-07 01:46:05 -0300

    #139 refactor: Mudança no tipo de retorno

--- src/presentation/controller/createEquipmentController.ts
+++ src/presentation/controller/createEquipmentController.ts
@@ -1,3 +1,7 @@
+import { Equipment } from "../../db/entities/equipment";
+import { EquipmentAcquisition } from "../../db/entities/equipment-acquisition";
+import { EquipmentBrand } from "../../db/entities/equipment-brand";
+import { Unit } from "../../domain/entities/unit";
 import { CreateEquipmentUseCase } from "../../useCases/createEquipment/createEquipmentUseCase";
 import { badRequest, HttpResponse, ok, serverError } from "../helpers";
 import { Controller } from "../protocols/controller";
@@ -42,75 +46,20 @@ type HttpRequest = {
 
 type Model =
   | Error
-  | {
-      tippingNumber: string;
+  | Equipment
 
-      serialNumber: string;
-
-      type: string;
-
-      status: string;
-
-      model: string;
-
-      description?: string;
-
-      initialUseDate: Date;
-
-      screenSize?: string;
-
-      invoiceNumber: string;
-
-      power?: string;
-
-      screenType?: string;
-
-      processor?: string;
-
-      storageType?: string;
-
-      storageAmount?: string;
-
-      brandId: string;
-
-      acquisitionId: string;
-
-      unitId: string;
-
-      ram_size?: string;
-    };
-export class CreateEquipmentController extends Controller {
-  constructor(private readonly createEquipment: CreateEquipmentUseCase) {
-    super();
-  }
-  async perform(params: HttpRequest): Promise<any> {
-    const response = await this.createEquipment.execute(params);
-    if (response.isSuccess) {
-      if (response.data !== null) {
-        return ok(response.data);
-      } else {
-        return badRequest;
-      }
-    } else {
-      return badRequest;
+export class CreateEquipmentController extends Controller{
+    constructor(private readonly createEquipment: CreateEquipmentUseCase){
+        super();
     }
-  }
+    async perform(params: HttpRequest):Promise<HttpResponse<Model>> {
+        const response = await this.createEquipment.execute(params)
+        if(response.isSuccess && response.data){
+            return ok(response.data)
+        }
+        else{
+            return serverError(response.error)
+        }
+    }
+
 }
-
-// export class CreateEquipmentController extends Controller{
-//     constructor(private readonly createEquipment: CreateEquipmentUseCase){
-//         super();
-//     }
-//     async perform(params: HttpRequest):Promise<HttpResponse<Model>> {
-//         const response = await this.createEquipment.execute(params)
-//         if(!response.isSuccess && response.data===null){
-//             console.log(response.data)
-//             return  serverError(response.error)
-
-//         }
-//         else{
-//             return serverError(response.error)
-//         }
-//     }
-
-// }
