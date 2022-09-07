3f6aa6a0a71c1889be8d8c5bf81531327d6751c8
pedroblome <joaopedroblome78@gmail.com>
2022-09-07 01:42:53 -0300

    #139 refactor: Adicionado rota para buscar unidades

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import { adaptExpressRoute as adapt } from './adapters/express-router'
 import { makeCreateOrderController } from './factories/controllers/create-order-service'
 import { makeCreateEquipmentController } from './factories/controllers/createEquipment'
+import { makeFindAllUnitsController } from './factories/controllers/findAllUnits'
+import { makeFindAllUnits } from './factories/useCases/findAllUnits'
 
 const routes = Router()
 
@@ -13,5 +15,11 @@ routes.post(
   '/createEquipment',
   adapt(makeCreateEquipmentController())
 )
+routes.get(
+  '/getAllUnits',
+  adapt(makeFindAllUnitsController())
+)
+
+
 
 export default routes
