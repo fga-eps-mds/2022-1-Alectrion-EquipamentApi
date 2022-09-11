d73b096e092f429348ddf29e147e22210e707836
jpcirqueira <jpcirqueira81@gmail.com>
2022-09-11 01:52:29 -0300

    Signed-off-by: jpcirqueira <jpcirqueira81@gmail.com> Co-authored-by: gleal17 <guilhermelml@gmail.com>

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { adaptExpressRoute as adapt } from './adapters/express-router'
 import { makeCreateOrderController } from './factories/controllers/create-order-service'
 import { makeCreateEquipmentController } from './factories/controllers/createEquipment'
 import { makeFindOneEquipmentController } from './factories/controllers/find-one-equipment-controller'
+import { makeFindOrderServiceController } from './factories/controllers/find-order-service'
 import { makeFindAllUnitsController } from './factories/controllers/findAllUnits'
 import { makeGetEquipmentController } from './factories/controllers/getEquipment'
 
@@ -16,4 +17,6 @@ routes.post('/createEquipment', adapt(makeCreateEquipmentController()))
 routes.get('/getAllUnits', adapt(makeFindAllUnitsController()))
 routes.get('/find', adapt(makeGetEquipmentController()))
 routes.get('/listOne', adapt(makeFindOneEquipmentController()))
+routes.get('/listOrderSerice', adapt(makeFindOrderServiceController()))
+
 export default routes
