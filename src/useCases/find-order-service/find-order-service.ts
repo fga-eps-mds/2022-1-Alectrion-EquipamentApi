964ab0ee5045d9dcdf74003aa0bdec194d8f8a8d
gleal17 <guilhermelml@gmail.com>
2022-09-11 01:20:17 -0300

    Signed-off-by: gleal17 <guilhermelml@gmail.com> Co-authored-by: João Pedro <jpcirqueira81@gmail.com>

---
+++ src/useCases/find-order-service/find-order-service.ts
@@ -0,0 +1,42 @@
+import { OrderService } from '../../domain/entities/order-service'
+import { OrderServiceRepositoryProtocol } from '../../repository/protocol/orderServiceRepositoryProtocol'
+import { UseCase, UseCaseReponse } from '../protocol/useCase'
+
+export class NotOSFoundError extends Error {
+  constructor() {
+    super('Order Service not found')
+    this.name = 'NotOSFoundError'
+  }
+}
+
+type FindOrderServiceUseCaseData = {
+  equipmentId: string
+  authorId: string
+  authorFunctionalNumber: string
+  destination: string
+  senderName: string
+  senderFunctionalNumber: string
+  date: string
+}
+
+export class FindOrderService
+  implements UseCase<FindOrderServiceUseCaseData, OrderService[]>
+{
+  constructor(private readonly osReposiory: OrderServiceRepositoryProtocol) {}
+  async execute(
+    query: FindOrderServiceUseCaseData
+  ): Promise<UseCaseReponse<OrderService[]>> {
+    const ordersServices = await this.osReposiory.findOrderServiceGeneric(query)
+    if (ordersServices !== null) {
+      return {
+        isSuccess: true,
+        data: ordersServices
+      }
+    } else {
+      return {
+        isSuccess: false,
+        error: new NotOSFoundError()
+      }
+    }
+  }
+}
