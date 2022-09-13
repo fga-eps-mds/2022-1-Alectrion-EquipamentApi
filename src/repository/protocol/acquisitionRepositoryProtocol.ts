00b352a2a2c7e1ad4ceaab6f59d7d5def7f2bf2b
pedroblome <joaopedroblome78@gmail.com>
2022-09-12 22:59:40 -0300

    #139 refactor: Implementado findOneByName acquisition

--- src/repository/protocol/acquisitionRepositoryProtocol.ts
+++ src/repository/protocol/acquisitionRepositoryProtocol.ts
@@ -1,11 +1,9 @@
 import { EquipmentAcquisition } from '../../db/entities/equipment-acquisition'
 
 export interface AcquisitionRepositoryProtocol {
-  create(
-    acqusition: EquipmentAcquisition
-  ): Promise<EquipmentAcquisition | undefined>
+  create(acqusition: EquipmentAcquisition): Promise<EquipmentAcquisition | null>
   findOne(acqusitionId: string): Promise<EquipmentAcquisition | null>
   findAll(): Promise<EquipmentAcquisition[] | null>
+  findOneByName(acquisitionName: string): Promise<EquipmentAcquisition | null>
 }
-
 export default AcquisitionRepositoryProtocol
