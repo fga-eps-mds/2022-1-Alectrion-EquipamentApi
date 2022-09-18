92914fae47e4cc190d28e8622f755a9e0f4202f5
Matheus Estanislau <matheus.estanislau7@gmail.com>
2022-09-18 13:35:14 -0300

    fix: adjust routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { checkAccessToken } from './middlewares/auth-middleware'
 import { Router } from 'express'
 import { adaptExpressRoute as adapt } from './adapters/express-router'
 import { makeCreateOrderController } from './factories/controllers/create-order-service'
@@ -5,16 +6,23 @@ import { makeCreateEquipmentController } from './factories/controllers/createEqu
 import { makeFindAllAcquisitionsController } from './factories/controllers/findAllAcquisitions'
 import { makeFindAllBrandsController } from './factories/controllers/findAllBrands'
 import { makeFindAllUnitsController } from './factories/controllers/findAllUnits'
+import { makeGetEquipmentController } from './factories/controllers/getEquipment'
+import { makeFindOrderServiceController } from './factories/controllers/find-order-service'
+import { makeFindOneEquipmentController } from './factories/controllers/find-one-equipment-controller'
 
 const routes = Router()
 
 routes.post(
   '/create-order-service/:equipmentId',
+  checkAccessToken,
   adapt(makeCreateOrderController())
 )
+routes.get('/find', adapt(makeGetEquipmentController()))
 routes.post('/createEquipment', adapt(makeCreateEquipmentController()))
 routes.get('/getAllUnits', adapt(makeFindAllUnitsController()))
 routes.get('/getAllBrands', adapt(makeFindAllBrandsController()))
 routes.get('/getAllAcquisitions', adapt(makeFindAllAcquisitionsController()))
+routes.get('/listOrderSerice', adapt(makeFindOrderServiceController()))
+routes.get('/listOne', adapt(makeFindOneEquipmentController()))
 
 export default routes
